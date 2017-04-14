1f90acb3580270a0f94131b70a54088c74e98911
Oleg Yanushevsky <asofi94@gmail.com>
2017-04-14 09:12:17 +0300

    added line drawing

---
+++ Assets/Line.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class Line : MonoBehaviour {
+
+    LineRenderer lineRenderer;
+    EdgeCollider2D edgeCollider;
+
+    List<Vector2> points;
+
+    private void Awake()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+        edgeCollider = GetComponent<EdgeCollider2D>();
+    }
+
+
+    public void UpdateLine(Vector2 mousePos)
+    {
+        if (points == null)
+        {
+            points = new List<Vector2>();
+            SetPoint(mousePos);
+            return;
+        }
+
+        if (Vector2.Distance(points.Last(), mousePos) > .1f)
+        {
+            SetPoint(mousePos);
+        }
+
+    }
+
+    void SetPoint(Vector2 point)
+    {
+        points.Add(point);
+
+        lineRenderer.positionCount = points.Count;
+        lineRenderer.SetPosition(points.Count - 1, point);
+        if(points.Count > 1)
+            edgeCollider.points = points.ToArray();
+    }
+}
---
+++ Assets/LineManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineManager : MonoBehaviour {
+
+    public GameObject linePrefab;
+    Line activeLine;
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject LineGO =  Instantiate(linePrefab);
+            activeLine = LineGO.GetComponent<Line>();
+        }
+
+        if (Input.GetMouseButtonUp(0))
+            activeLine = null;
+
+        if(activeLine != null)
+        {
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            activeLine.UpdateLine(mousePos);
+        }
+
+	}
+}
