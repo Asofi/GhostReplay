f1939be47f5cf9c2b5a1a3512e2daa7b3414bda9
Oleg Yanushevsky <o.yanushevsky@gmail.com>
2017-04-18 10:36:03 +0300

    smthng

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    Rigidbody2D mRigidBody;
+
+	// Use this for initialization
+	void Start () {
+        mRigidBody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(1))
+        {
+            mRigidBody.bodyType = RigidbodyType2D.Dynamic;
+        }
+	}
+}
